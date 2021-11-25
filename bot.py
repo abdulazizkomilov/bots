cf3b53498cfe2d18dc70c6e5584f5cf6236cd977
abulaziz <abdulazizkomilov2001@gmail.com>
2021-11-25 22:53:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,8 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+
+
     await message.reply("Hi!\nWelcome to wikibot!")
     await message.reply("Bu bot siz kiritgan so'zga doir malumotlarni wikipediadan topib beradi.")
 
