80ed63076884f544144e8f03279d9ff7921245c7
abulaziz <abdulazizkomilov2001@gmail.com>
2021-11-27 20:10:29 +0500

    test_api_request

--- 2_bot/test.py
+++ 2_bot/test.py
@@ -1,4 +1,13 @@
 import requests
 from pprint import pprint as print
 
-API_KEY = ''
+API_KEY = '3f220948906c87f9d3642fe9'
+
+currency = 'USD'
+url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{currency}/UZS"
+
+response = requests.get(url)
+# print(response.status_code)
+# print(response.json())
+kurs = response.json()['conversion_rate']
+print(f"Bugungi kurs: 1AQSH dollori {kurs}")
