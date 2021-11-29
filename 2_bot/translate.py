d8c3ba9ed9dfc2e3f497b06a2d87b1af5d345668
abulaziz <abdulazizkomilov2001@gmail.com>
2021-11-29 23:25:34 +0500

    translate bot

--- 2_bot/translate.py
+++ 2_bot/translate.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint as print
 
 
-app_id = "f42a879e"
+app_id = "'f42a879e'"
 app_key = "2dcf1c6c77083320619da4f37f85e50e"
 
 language = "en-gb"
@@ -10,7 +10,7 @@ word_id = "python"
 
 url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
 
-r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+r = requests.get(url, headers={"app_id": 'f42a879e', "app_key": '2dcf1c6c77083320619da4f37f85e50e'})
 print(r.status_code)
-res = r.json()
-print(res)
+# res = r.json()
+# print(res)
