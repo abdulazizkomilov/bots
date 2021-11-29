44c15c29cde1567e5b0350777d732c7a102f0825
abulaziz <abdulazizkomilov2001@gmail.com>
2021-11-29 20:04:05 +0500

    quran bot

--- 2_bot/quron.py
+++ 2_bot/quron.py
@@ -1,3 +1,15 @@
 import requests
 from pprint import pprint as print
 
+surah = 1
+ayat = 4
+tafsir = 'uzb-muhammadsodikmu'
+language = 'ar'
+
+url_sura = f"http://api.alquran.cloud/v1/edition/language/{language}"
+
+response = requests.get(url_sura)
+# print(response.status_code)
+print(response.json())
+# kurs = response.json()['conversion_rate']
+print(response)
---
+++ 2_bot/translate.py
@@ -0,0 +1,16 @@
+import requests
+from pprint import pprint as print
+
+
+app_id = "f42a879e"
+app_key = "2dcf1c6c77083320619da4f37f85e50e"
+
+language = "en-gb"
+word_id = "python"
+
+url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+
+r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+print(r.status_code)
+res = r.json()
+print(res)
