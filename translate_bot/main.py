a5a2d3f5bf34a80a1afe27452fb8a60307d273af
abulaziz <abdulazizkomilov2001@gmail.com>
2021-12-01 23:22:22 +0500

    deploy

--- translate_bot/main.py
+++ translate_bot/main.py
@@ -7,6 +7,7 @@ translator = Translator()
 
 API_TOKEN = '2113362402:AAHOISfKIJDQrnBleEGDXjA1SdbpNFy56f0'
 
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
@@ -14,21 +15,33 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand("start", "Botni ishga tushuring"),
+            types.BotCommand("help", "Yordam"),
+        ]
+    )
+
+async def on_startup(dispatcher):
+    await set_default_commands(dispatcher)
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
 
-    await message.reply("Salom!\nMen sizga tarjimonlik qilaman!\nMarxamat foydalaning.")
+    await message.reply("Tarjimon Botga Xush Kelibsiz!")
 
 @dp.message_handler(commands=['help'])
 async def send_helper(message: types.Message):
 
-    await message.reply("Bu bot sizga tarjimonlik qiladi. Siz uz-en yoki en-uz da foydalanishingiz mumkin.")
+    await message.reply("Foydalanish uchun tarjima qilmoqchi bo`lgan so`zni yoki gapni kiriting. \nSiz uz-en yoki en-uz da foydalanishingiz mumkin.")
 
 @dp.message_handler()
 async def tarjimon(message: types.Message):
     lang = translator.detect(message.text).lang
     dest = 'uz' if lang == 'en' else 'en'
     await message.reply(translator.translate(message.text, dest).text)
+
 
 
 if __name__ == '__main__':
