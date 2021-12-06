810d29e9fdf7e7375c2d69a872d7f1aa6baf9b03
abulaziz <abdulazizkomilov2001@gmail.com>
2021-12-06 19:09:13 +0500

    commands

--- 3_bot/handlers/users/__init__.py
+++ 3_bot/handlers/users/__init__.py
@@ -1,3 +1,4 @@
+from . import commandsFiltr
 from . import help
 from . import start
 from . import echo
---
+++ 3_bot/handlers/users/commandsFiltr.py
@@ -0,0 +1,7 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import Command
+from loader import dp
+
+@dp.message_handler(Command('til'))
+async def changeLanguage(message: types.Message):
+    await message.answer(f"Til o`zgardi")
